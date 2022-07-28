a6f31ea9ad9cf05daa333e5dfa26c32946eb6b36
mishra811 <mishrakundan073@gmail.com>
2022-07-28 17:04:12 +0530

    added wallet connect

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,10 +1,135 @@
 import "../styles/globals.css";
-import AppContext from "./../.context";
-
+import AppContext from "../context";
+import { useEffect, useState } from "react";
+import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
+import WalletConnect from "@walletconnect/web3-provider";
+import Web3Modal from "web3modal";
+import { ethers } from "ethers";
 function MyApp({ Component, pageProps }) {
+  const [provider, setProvider] = useState();
+  const [library, setLibrary] = useState();
+  const [account, setAccount] = useState();
+  const [network, setNetwork] = useState();
+  const [chainId, setChainId] = useState();
+  const [isReturningUser, setIsReturningUser] = useState(false);
+  const connectWallet = async () => {
+    try {
+      const providerOptions = {
+        coinbasewallet: {
+          package: CoinbaseWalletSDK,
+          options: {
+            appName: "Web 3 Modal Demo",
+          },
+        },
+        walletconnect: {
+          package: WalletConnect,
+          options: {
+            infuraId: "27e484dcd9e3efcfd25a83a78777cdf1",
+          },
+        },
+      };
+      const web3Modal = new Web3Modal({
+        providerOptions, // required
+      });
+      const provider = await web3Modal.connect();
+      const library = new ethers.providers.Web3Provider(provider);
+
+      const accounts = await library.listAccounts();
+      const network = await library.getNetwork();
+      setProvider(provider);
+      setLibrary(library);
+      if (accounts) setAccount(accounts[0]);
+      setNetwork(network);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getCurrentAccount = async () => {
+    window.ethereum.on("accountsChanged", (accounts) => {
+      if (accounts.length > 0) {
+        setAccount(accounts[0]);
+        console.log(account);
+      } else {
+        setAccount("");
+      }
+    });
+  };
+
+  const getConnectedWallet = async () => {
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length) {
+      setAccount(accounts[0]);
+    } else {
+      console.log("no accounts found");
+    }
+  };
+
+  const switchAccounts = async () => {
+    const walletAddress = await window.ethereum.request({
+      method: "eth_requestAccounts",
+      params: [
+        {
+          eth_accounts: {},
+        },
+      ],
+    });
+
+    if (!isReturningUser) {
+      // Runs only they are brand new, or have hit the disconnect button
+      await window.ethereum.request({
+        method: "wallet_requestPermissions",
+        params: [
+          {
+            eth_accounts: {},
+          },
+        ],
+      });
+    }
+  };
+
+  useEffect(() => {
+    getCurrentAccount();
+    if (provider?.on) {
+      const handleAccountsChanged = (accounts) => {
+        setAccount(accounts);
+      };
+
+      const handleChainChanged = (chainId) => {
+        setChainId(chainId);
+      };
+
+      const handleDisconnect = () => {
+        disconnect();
+        setIsReturningUser(true);
+      };
+
+      provider.on("accountsChanged", handleAccountsChanged);
+      provider.on("chainChanged", handleChainChanged);
+      provider.on("disconnect", handleDisconnect);
+
+      return () => {
+        if (provider.removeListener) {
+          provider.removeListener("accountsChanged", handleAccountsChanged);
+          provider.removeListener("chainChanged", handleChainChanged);
+          provider.removeListener("disconnect", handleDisconnect);
+        }
+      };
+    }
+  }, [provider]);
+
+  useEffect(() => {
+    getConnectedWallet();
+    getCurrentAccount();
+  }, []);
   return (
-    <AppContext.Provider>
-      <Component {...pageProps} />
+    <AppContext.Provider value={{ account }}>
+      <Component
+        {...pageProps}
+        connectWallet={connectWallet}
+        switchAccounts={switchAccounts}
+      />
     </AppContext.Provider>
   );
 }
