42a96c05a1288f31da797494a9ad8da22e4ffd85
Faisal Ahmed <faisalahmedfarooq46@gmail.com>
2022-08-01 23:57:40 +0530

    fix: added constants

---
+++ frontend/constants/addresses.js
@@ -0,0 +1,9 @@
+import contract from './contract_abi.json'
+
+const CONTRACT_ADDRESS = " 0xf2AE64C2821797f55ba80d04beDE4e7FFF35d9B9";
+const CONTRACT_ABI = contract.abi;
+
+export{
+    CONTRACT_ADDRESS,
+    CONTRACT_ABI
+};
