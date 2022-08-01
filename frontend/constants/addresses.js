1fc8c50dcdeb418a3e710e5eb28957e6b55e05f7
mishra811 <mishrakundan073@gmail.com>
2022-08-02 01:26:36 +0530

    added ipfs scripts for uploading data

--- frontend/constants/addresses.js
+++ frontend/constants/addresses.js
@@ -1,9 +1,6 @@
-import contract from './contract_abi.json'
+import contract from "./contract_abi.json";
 
-const CONTRACT_ADDRESS = " 0xf2AE64C2821797f55ba80d04beDE4e7FFF35d9B9";
+const CONTRACT_ADDRESS = "0xbeDE3a4493640c5EFFFF96a936AE8c987651Ca10";
 const CONTRACT_ABI = contract.abi;
 
-export{
-    CONTRACT_ADDRESS,
-    CONTRACT_ABI
-};
+export { CONTRACT_ADDRESS, CONTRACT_ABI };
