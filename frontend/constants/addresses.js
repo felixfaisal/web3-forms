4f74ce2ac043f327bfb184348c5174c9f6ec8fb9
Faisal Ahmed <faisalahmedfarooq46@gmail.com>
2022-08-04 15:04:04 +0530

    fix: updated abi json and address constants

--- frontend/constants/addresses.js
+++ frontend/constants/addresses.js
@@ -1,6 +1,6 @@
 import contract from "./contract_abi.json";
 
-const CONTRACT_ADDRESS = "0xbeDE3a4493640c5EFFFF96a936AE8c987651Ca10";
+const CONTRACT_ADDRESS = "0x2Ae221CE6FC9D5b5Eb6d526F14658Ac640eF7f1b";
 const CONTRACT_ABI = contract.abi;
 
 export { CONTRACT_ADDRESS, CONTRACT_ABI };
