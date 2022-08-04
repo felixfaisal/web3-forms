ac0000b65c4d8a3f0bc23e08ae16f53dc1317907
Faisal Ahmed <faisalahmedfarooq46@gmail.com>
2022-08-04 15:20:26 +0530

    fix: update contract address

--- frontend/constants/addresses.js
+++ frontend/constants/addresses.js
@@ -1,6 +1,6 @@
 import contract from "./contract_abi.json";
 
-const CONTRACT_ADDRESS = "0x2Ae221CE6FC9D5b5Eb6d526F14658Ac640eF7f1b";
+const CONTRACT_ADDRESS = "0xfFb59b9Fb0cDF5567e7f8aD908e6960233fF6991";
 const CONTRACT_ABI = contract.abi;
 
 export { CONTRACT_ADDRESS, CONTRACT_ABI };
