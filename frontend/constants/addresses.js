67167c0de67b7a01ffa7b2915de8d321e3f93ea3
Faisal Ahmed <faisalahmedfarooq46@gmail.com>
2022-08-06 23:49:50 +0530

    fix: add new smart contract deployment

--- frontend/constants/addresses.js
+++ frontend/constants/addresses.js
@@ -1,6 +1,6 @@
 import contract from "./contract_abi.json";
 
-const CONTRACT_ADDRESS = "0xfFb59b9Fb0cDF5567e7f8aD908e6960233fF6991";
+const CONTRACT_ADDRESS = "0xe0585918a689Eaaf528814a89c3083012E28414a";
 const CONTRACT_ABI = contract.abi;
 
 export { CONTRACT_ADDRESS, CONTRACT_ABI };
