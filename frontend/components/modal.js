a4cbc48ad8574309a970f29708e16b438b8ed61d
mishra811 <mishrakundan073@gmail.com>
2022-08-07 02:54:33 +0530

    addded response data

--- frontend/components/modal.js
+++ frontend/components/modal.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Modal({ setShowModal, showModal }) {
+export default function Modal({ setShowModal, showModal, modalResponse }) {
   return (
     <>
       {/* <button
@@ -18,7 +18,7 @@ export default function Modal({ setShowModal, showModal }) {
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">Modal Title</h3>
+                  <h3 className="text-3xl font-semibold">Your Response</h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => setShowModal(false)}
@@ -31,11 +31,7 @@ export default function Modal({ setShowModal, showModal }) {
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
                   <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
+                    {modalResponse}
                   </p>
                 </div>
                 {/*footer*/}
